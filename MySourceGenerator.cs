278cab8302b21246cc3799861443e7d573c75292
Andy <musictopia2@hotmail.com>
2021-12-13 15:40:36 -0700

    Major update to use incremental source generators. Plus used the new version of the generator that requires interface to be sent in.

--- MySourceGenerator.cs
+++ MySourceGenerator.cs
@@ -2,14 +2,13 @@
 //can't use global namespaces.   otherwise, the getclasssymbol does not work properly.
 namespace HelpersExperiment.HelpersGeneratorLibrary;
 [Generator] //this is important so it knows this class is a generator which will generate code for a class using it.
-public class MySourceGenerator : ISourceGenerator
+public class MySourceGenerator : IIncrementalGenerator
 {
-    public void Execute(GeneratorExecutionContext context)
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        context.BuildSourceCode();
-
-    }
-    public void Initialize(GeneratorInitializationContext context)
-    {
+        context.RegisterPostInitializationOutput(c =>
+        {
+            c.CreateCustomSource().BuildSourceCode();
+        });
     }
 }
