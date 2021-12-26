4e96ab881101532cf6bd915a8c5cf5dac85ee4a8
Andy <musictopia2@hotmail.com>
2021-12-26 07:50:37 -0700

    Extra improvements to the source code string builder. First, supports up to 8 intent levels with no problems with performance. Also, added some interfaces for code blocks and for writing lines. Finally, added a new method for code blocks so if there are lots of complex code blocks, makes it easier to read.

--- SourceCodeStringBuilder.cs
+++ SourceCodeStringBuilder.cs
@@ -1,6 +1,18 @@
 ﻿using System.Text;
 namespace CommonSourceGeneratorsHelpers;
-internal class SourceCodeStringBuilder
+public interface IWriter
+{
+    IWriter Write(object obj);
+    IWriter AppendDoubleQuote(Action<IWriter> action);
+}
+public interface ICodeBlock
+{
+    ICodeBlock WriteLine(string text);
+    ICodeBlock WriteLine(Action<IWriter> action);
+    ICodeBlock WriteCodeBlock(Action<ICodeBlock> action, bool endSemi = false, bool alsoBlankLine = false);
+    ICodeBlock AppendEmptyLine();
+}
+public class SourceCodeStringBuilder : IWriter, ICodeBlock
 {
     private const string DoubleQuote = "\"";
     private const string _oneSpace = "    ";
@@ -10,6 +22,7 @@ internal class SourceCodeStringBuilder
     private const string _fiveSpaces = "                    ";
     private const string _sixSpaces = "                        ";
     private const string _sevenSpaces = "                            ";
+    private const string _eightSpaces = "                                ";
     int _indentLevel = 0;
 
     //readonly StringBuilder _currentLine = new();
@@ -67,10 +80,10 @@ internal class SourceCodeStringBuilder
         InnerDecreaseIndent();
         return this;
     }
-    public SourceCodeStringBuilder WriteLine(Action action)
+    public SourceCodeStringBuilder WriteLine(Action<IWriter> action)
     {
         Indent();
-        action.Invoke(); //so i can break apart what is being written.
+        action.Invoke(this); //so i can break apart what is being written.
         _builds.AppendLine();
         _needsNewLine = false;
         return this;
@@ -117,24 +130,50 @@ internal class SourceCodeStringBuilder
         if (_indentLevel == 6)
         {
             _builds.Append(_sixSpaces);
+            return;
         }
         if (_indentLevel == 7)
         {
             _builds.Append(_sevenSpaces);
+            return;
         }
-        throw new Exception("Only 7 spaces supported at the most");
+        if (_indentLevel == 8)
+        {
+            _builds.Append(_eightSpaces);
+            return;
+        }
+        throw new Exception("Only 8 spaces supported at the most");
     }
-    public SourceCodeStringBuilder Write(object obj)
+    private SourceCodeStringBuilder Write(object obj)
     {
         _needsNewLine = true;
         _builds.Append(obj);
         return this;
+    }
+    IWriter IWriter.Write(object obj)
+    {
+        return Write(obj);
     }
     public override string ToString()
     {
         return _builds.ToString();
     }
     #region Custom Stuff
+    public SourceCodeStringBuilder WriteCodeBlock(Action<ICodeBlock> action, bool endSemi = false, bool alsoBlankLine = false)
+    {
+        StartBlock();
+        action.Invoke(this);
+        if (endSemi == false)
+        {
+            EndBlock(alsoBlankLine);
+            return this;
+        }
+        DecreaseIndent().EnsureEmptyLine();
+        Indent();
+        Write("};"); //end semi means to also have a blank line.
+        _builds.AppendLine();
+        return this;
+    }
     public SourceCodeStringBuilder StartBlock()
     {
         Indent();
@@ -153,13 +192,37 @@ internal class SourceCodeStringBuilder
         }
         return this;
     }
-    public SourceCodeStringBuilder AppendDoubleQuote(Action action)
+    public SourceCodeStringBuilder AppendDoubleQuote(Action<SourceCodeStringBuilder> action)
     {
         Write(DoubleQuote);
-        action.Invoke();
+        action.Invoke(this);
         Write(DoubleQuote);
         return this;
     }
-    
+    ICodeBlock ICodeBlock.WriteLine(string text)
+    {
+        return WriteLine(text);
+    }
+
+    ICodeBlock ICodeBlock.WriteLine(Action<IWriter> action)
+    {
+        return WriteLine(action);
+    }
+    ICodeBlock ICodeBlock.WriteCodeBlock(Action<ICodeBlock> action, bool endSemi, bool alsoBlankLine)
+    {
+        return WriteCodeBlock(action, endSemi, alsoBlankLine);
+    }
+    public SourceCodeStringBuilder AppendEmptyLine()
+    {
+        return WriteLine("");
+    }
+    ICodeBlock ICodeBlock.AppendEmptyLine()
+    {
+        return AppendEmptyLine();
+    }
+    IWriter IWriter.AppendDoubleQuote(Action<IWriter> action)
+    {
+        return AppendDoubleQuote(action);
+    }
     #endregion
 }
